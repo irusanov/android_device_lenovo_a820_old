5c87ddcb64125f151cedc22c7d4ff4b335cb07a1
Ivan Rusanov <ivan.b.rusanov@mail.com>
2015-04-21 00:59:12 +0300

    Push device tree

---
+++ BoardConfig.mk
@@ -0,0 +1,83 @@
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DEVICE_FOLDER := device/wiko/stairway
+
+-include vendor/wiko/stairway/BoardConfigVendor.mk
+
+# board
+TARGET_BOARD_PLATFORM := mt6589
+TARGET_ARCH := arm
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a7
+
+# blob hacks
+COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+
+# BT
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_MTK := true
+BOARD_BLUETOOTH_DOES_NOT_USE_RFKILL := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lenovo/a820/bluetooth
+
+# power
+TARGET_POWERHAL_VARIANT := cm
+
+# boot
+TARGET_NO_BOOTLOADER := true
+BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_FOLDER)/boot.mk
+
+# EGL settings
+BOARD_EGL_CFG := device/lenovo/a820/egl.cfg
+USE_OPENGL_RENDERER := true
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+
+# kernel
+TARGET_PREBUILT_KERNEL := $(DEVICE_FOLDER)/prebuilt/kernel
+
+# partition info
+BOARD_BOOTIMAGE_PARTITION_SIZE := 6291456
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1611661312
+BOARD_USERDATAIMAGE_PARTITION_SIZE:= 4830789632
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 6291456
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_FLASH_BLOCK_SIZE := 512
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_FOLDER)/root/fstab.mt6589
+TARGET_PREBUILT_RECOVERY_KERNEL := $(DEVICE_FOLDER)/recovery/kernel
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# wifi
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_WLAN_DEVICE := mediatek
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_mt66xx
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_mt66xx
+WIFI_DRIVER_FW_PATH_PARAM:="/dev/wmtWifi"
+WIFI_DRIVER_FW_PATH_STA:=STA
+WIFI_DRIVER_FW_PATH_AP:=AP
+WIFI_DRIVER_FW_PATH_P2P:=P2P
+
+# telephony
+BOARD_RIL_CLASS := ../../../device/lenovo/a820/ril/
+
+# allow more than one lun file
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun%d/file"
---
+++ cm.mk
@@ -0,0 +1,28 @@
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit device configuration
+$(call inherit-product, device/lenovo/a820/full_a820.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
+
+PRODUCT_NAME := cm_a820
+PRODUCT_DEVICE := lenovo
+PRODUCT_MODEL := Lenovo A820
+PRODUCT_RELEASE_NAME := A820
+PRODUCT_BRAND := Android
+PRODUCT_MANUFACTURER := Lenovo
